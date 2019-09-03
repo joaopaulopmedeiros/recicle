7483247037f3196d8a3ecbff8bd85d021f4311b2
joao paulo <joaopaulopmedeiros@gmail.com>
2019-09-02 21:48:45 -0300

    fixing links by controller on user_public about finished

--- application/views/user_public/about.php
+++ application/views/user_public/about.php
@@ -1,4 +1,4 @@
-    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(../images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(<?php echo base_url();?>assets/template/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
 
       <div class="container">
         <div class="row align-items-center justify-content-center text-center">
@@ -16,7 +16,7 @@
         <div class="row mb-5">
           
           <div class="col-md-5 ml-auto mb-5 order-md-2" data-aos="fade">
-            <img src="../images/img_1.jpg" alt="Image" class="img-fluid rounded">
+            <img src="<?php echo base_url();?>assets/template/images/img_1.jpg" alt="Image" class="img-fluid rounded">
           </div>
           <div class="col-md-6 order-md-1" data-aos="fade">
             <div class="text-left pb-1 border-primary mb-4">
@@ -65,7 +65,7 @@
         <div class="row">
           <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
             <div class="person">
-              <img src="../images/person_2.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
+              <img src="<?php echo base_url();?>assets/template/images/person_2.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
               <h3>Débora Samara</h3>
               <p class="position text-muted">Co-Founder, President</p>
               <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi at consequatur unde molestiae quidem provident voluptatum deleniti quo iste error eos est praesentium distinctio cupiditate tempore suscipit inventore deserunt tenetur.</p>
@@ -80,7 +80,7 @@
           </div>
            <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
             <div class="person">
-              <img src="../images/person_1.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
+              <img src="<?php echo base_url();?>assets/template/images/person_1.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
               <h3>Fabiana Pereira</h3>
               <p class="position text-muted">Co-Founder, President</p>
               <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi at consequatur unde molestiae quidem provident voluptatum deleniti quo iste error eos est praesentium distinctio cupiditate tempore suscipit inventore deserunt tenetur.</p>
@@ -94,7 +94,7 @@
           </div>
           <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
             <div class="person">
-              <img src="../images/person_3.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
+              <img src="<?php echo base_url();?>assets/template/images/person_3.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
               <h3>Higor Nascimento</h3>
               <p class="position text-muted">Co-Founder, COO</p>
               <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi at consequatur unde molestiae quidem provident voluptatum deleniti quo iste error eos est praesentium distinctio cupiditate tempore suscipit inventore deserunt tenetur.</p>
@@ -108,7 +108,7 @@
           </div>
           <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="300">
             <div class="person">
-              <img src="../images/person_4.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
+              <img src="<?php echo base_url();?>assets/template/images/person_4.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
               <h3>João Paulo</h3>
               <p class="position text-muted">Marketing</p>
               <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi at consequatur unde molestiae quidem provident voluptatum deleniti quo iste error eos est praesentium distinctio cupiditate tempore suscipit inventore deserunt tenetur.</p>
