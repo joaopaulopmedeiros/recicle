09ba50b2efc1a29f4801e5ccdda0e225d440ebb9
joao paulo <joaopaulopmedeiros@gmail.com>
2019-09-02 22:52:37 -0300

    change directory structure to minimize lenght of project. Webpages called in a easier way

--- application/controllers/user_cooperativa.php
+++ application/controllers/user_cooperativa.php
@@ -9,11 +9,34 @@ class User_cooperativa extends CI_Controller {
 		$this->load->view('user_cooperativa/index');
 		$this->load->view('user_cooperativa/footer');
 	}
+	public function showViewAbout()
+	{
+		$this->load->view('user_cooperativa/header');
+		$this->load->view('about');
+		$this->load->view('user_cooperativa/footer');
+	}	
+	public function showViewContact()
+	{
+		$this->load->view('user_cooperativa/header');
+		$this->load->view('contact');
+		$this->load->view('user_cooperativa/footer');
+	}
 	public function showViewDesafios()
 	{
 		$this->load->view('user_cooperativa/header');
-		$this->load->view('user_cooperativa/desafios');
+		$this->load->view('desafios');
 		$this->load->view('user_cooperativa/footer');
 	}
-	
+	public function showViewLogin()
+	{
+		$this->load->view('user_cooperativa/header');
+		$this->load->view('login');
+		$this->load->view('user_cooperativa/footer');
+	}
+	public function showViewEcopontos()
+	{
+		$this->load->view('user_cooperativa/header');
+		$this->load->view('ecopontos');
+		$this->load->view('user_cooperativa/footer');
+	}
 }
