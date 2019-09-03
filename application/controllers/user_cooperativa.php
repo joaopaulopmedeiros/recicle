fbe0f8aff2d3f552b3b77e2e53949b0970377ca5
joao paulo <joaopaulopmedeiros@gmail.com>
2019-09-02 22:39:25 -0300

    user_cooperativa/index parcial integrated to CI

--- application/controllers/user_cooperativa.php
+++ application/controllers/user_cooperativa.php
@@ -9,34 +9,11 @@ class User_cooperativa extends CI_Controller {
 		$this->load->view('user_cooperativa/index');
 		$this->load->view('user_cooperativa/footer');
 	}
-	public function showViewAbout()
-	{
-		$this->load->view('user_cooperativa/header');
-		$this->load->view('user_cooperativa/about');
-		$this->load->view('user_cooperativa/footer');
-	}	
-	public function showViewContact()
-	{
-		$this->load->view('user_cooperativa/header');
-		$this->load->view('user_cooperativa/contact');
-		$this->load->view('user_cooperativa/footer');
-	}
 	public function showViewDesafios()
 	{
 		$this->load->view('user_cooperativa/header');
 		$this->load->view('user_cooperativa/desafios');
 		$this->load->view('user_cooperativa/footer');
 	}
-	public function showViewLogin()
-	{
-		$this->load->view('user_cooperativa/header');
-		$this->load->view('user_cooperativa/login');
-		$this->load->view('user_cooperativa/footer');
-	}
-	public function showViewEcopontos()
-	{
-		$this->load->view('user_cooperativa/header');
-		$this->load->view('user_cooperativa/ecopontos');
-		$this->load->view('user_cooperativa/footer');
-	}
+	
 }
---
+++ application/views/user_cooperativa/index.php
@@ -0,0 +1,119 @@
+    <div class="site-blocks-cover overlay" style="background-image: url(<?php echo base_url();?>assets/template/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+          <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
+            <h1 class="text-white font-weight-light mb-5 text-uppercase font-weight-bold">Bonificação por meio do descarte correto</h1>
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    
+
+    <div class="container">
+      <div class="row align-items-center no-gutters align-items-stretch overlap-section">
+        <div class="col-md-4">
+          <div class="feature-1 pricing h-100 text-center">
+            <div class="icon">
+              <span class="icon-puzzle-piece"></span>
+            </div>
+            <a href="desafios.html">
+              <h2 class="my-4 heading">Cadastrar novo desafio</h2>
+            </a>
+            <p>Adicione novos desafios.</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="feature-3 pricing h-100 text-center">
+            <div class="icon">
+              <span class="icon-settings"></span>
+            </div>
+            <a href="my-account.html">
+              <h2 class="my-4 heading">Minha Conta</h2>
+            </a>
+            <p>Edite as configurações da sua conta do Recicle.</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="feature-1 pricing h-100 text-center">
+            <div class="icon">
+              <span class="icon-map-marker"></span>
+            </div>
+            <a href="ecopontos.html">
+              <h2 class="my-4 heading">Ecopontos</h2>
+            </a>
+            <p>Veja as cooperativas proximas de você.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  
+    
+    <div class="site-section">
+      <div class="container">
+
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary">Bonificações</h2>
+          </div>
+        </div>
+
+        <div class="row justify-content-around">
+
+          <div class="col-md-5 align-self-center">
+            <div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+            </div>
+          </div>
+
+          <div class="col-md-4 slide-one-item owl-carousel">
+            <div class="d-flex justify-content-center">
+              <figure style="width:250px;">
+                <img src="https://pbs.twimg.com/profile_images/661888424073367552/pSK6r19h_400x400.png" alt="Image">
+              </figure>
+            </div>
+
+            <div class="d-flex justify-content-center">
+              <figure style="width:250px;">
+                <img src="<?php echo base_url();?>assets/template/images/xbox.png" alt="Image">
+              </figure>
+            </div>
+
+            <div class="d-flex justify-content-center">
+              <figure style="width:250px;">
+                <img src="<?php echo base_url();?>assets/template/images/spot.png" alt="Image">
+              </figure>
+            </div>
+
+            <div class="d-flex justify-content-center">
+              <figure style="width:250px;">
+                <img src="<?php echo base_url();?>assets/template/images/uber.png" alt="Image">
+              </figure>
+            </div>
+
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+
+
+    <div class="site-section" style="background-color: #98989833;">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary">Lixos mais descartados</h2>
+          </div>
+        </div>
+        <div class="row align-items-stretch">
+          <div class="d-flex justify-content-center">
+          <figure style="width: 90%;">
+              <img src="<?php echo base_url();?>assets/template/images/lixo.png" alt="Imagem com os lixos mais descartados" class="img-fluid">
+            </figure>
+          </div>
+
+        </div>
+      </div>
+    </div>
