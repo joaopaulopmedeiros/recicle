45581170f5488ad3424d177dedecf7cae1e29cf9
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-10-09 16:23:22 -0300

    alteração no formulário de cadastro

--- application/views/login.php
+++ application/views/login.php
@@ -9,93 +9,153 @@
           </div>
         </div>
       </div>
-    </div>  
+    </div>
 
-    
+    <script>
+      $('.show_hide').click(function(){
+        $(this).next('.slidingDiv').slideToggle();
+        return false;
+      });
+    </script>
+
     <div class="site-section bg-light">
       <div class="container">
         <div class="row">
           <div class="col-md-6 mb-5">
-
-            <form action="<?=base_url()?>user_cidadao/cadastrar" class="p-5 bg-white">
+            <form class="p-5 bg-white">
              
               <h3>Cadastre-se</h3>
-              <p> Insira aqui seus dados para que possamos fazer o seu cadastro!</p>
-              <div class="row form-group">
+              <p>Selecione o tipo de usuário: </p>
+              
+              <div class="row justify-content-around">
+                <div class="btn-group btn-group-toggle" data-toggle="buttons" id="accordion">
+                  <label class="btn btn-primary mt-1"><input type="radio" name="btnCidadao" value="cidadao" data-toggle="collapse" data-parent="#accordion" href="#cidadao">Cidadão</label>
 
-                <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="text-black" for="fname">Nome</label>
-                  <input type="text" id="fname" class="form-control">
-                </div>
-              </div>
+                  <!--<button class="btn btn-primary mt-1" id="btnCidadao" type="button" data-toggle="collapse" data-target="#cidadao" aria-expanded="false" aria-controls="cidadao">Cidadão</button>-->
 
-                <div class="row form-group">
-                
-                <div class="col-md-12">
-                  <label class="text-black" for="lname">E-mail</label>
-                  <input type="email" id="email" class="form-control">
-                </div>
-              </div>
-
-               <div class="row form-group">
-
-                <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="text-black" for="fname">CPF</label>
-                  <input type="text" id="" class="form-control">
-                </div>
-              </div>
-
-                <div class="row form-group">
-
-                <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="text-black" for="fname">CEP</label>
-                  <input type="text" id="" class="form-control">
-                </div>
-              </div>
-
-              <div class="row form-group">
-                
-                <div class="col-md-6">
-                  <label class="text-black" for="email">Senha</label> 
-                  <input type="password" id="" class="form-control">
-                </div>
-                 <div class="col-md-6">
-                  <label class="text-black" for="subject">Confirmar senha</label> 
-                  <input type="password" id="" class="form-control">
-                </div>
-              </div>
-
-              <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="usuario" id="usuario" value="usuario" checked>
-                  <label class="form-check-label" for="usuario">
-                    Cidadão
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="cooperativa" id="cooperativa" value="cooperativa">
-                  <label class="form-check-label" for="cooperativa">
-                    Cooperativa
-                  </label>
-                </div>
+                  <label class="btn btn-primary mt-1"><input type="radio" name="btnCriadorDesafio" value="criadorDesafio" data-toggle="collapse" data-parent="#accordion" href="#criadorDesafio">Criador de Desafios</label>
+                  
+                  <!--<button class="btn btn-primary mt-1 "id="btnCriadorDesafio" type="button" data-toggle="collapse" data-target="#criadorDesafio" aria-expanded="false" aria-controls="criadorDesafio">Criador de Desafios</button>-->
+                </div> 
               </div>
               
+              <div class="accordion">
+                <div class="collapse mt-5" id="cidadao">
+                  <div class="row form-group">
+                    <div class="col-md-12 mb-3 mb-md-0">
+                      <label class="text-black" for="nome">Nome</label>
+                      <input type="text" id="nome" name="nome" class="form-control">
+                    </div>
+                  </div>
 
-              <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                  <label class="form-check-label" for="invalidCheck">
-                    Concordo com os <a href="">termos e condições</a>
-                  </label>
-                  <div class="invalid-feedback">
-                    Você deve concordar, antes de continuar.
+                  <div class="row form-group">
+                    <div class="col-md-12">
+                      <label class="text-black" for="lname">E-mail</label>
+                      <input type="email" id="email" name="email" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-12 mb-3 mb-md-0">
+                      <label class="text-black" for="fname">CPF</label>
+                      <input type="text" id="cpf" name="cpf" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-12 mb-3 mb-md-0">
+                      <label class="text-black" for="fname">CEP</label>
+                      <input type="text" id="cep" name="cep" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-6">
+                      <label class="text-black" for="email">Senha</label> 
+                      <input type="password" id="senha" name="senha" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                      <label class="text-black" for="subject">Confirmar senha</label> 
+                      <input type="password" id="confirmarSenha" name="confirmarSenha" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="" id="termos" name="termos" required>
+                      <label class="form-check-label" for="termos">
+                        Concordo com os <a href="">termos e condições</a>
+                      </label>
+                      <div class="invalid-feedback">
+                        Você deve concordar, antes de continuar.
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-12">
+                      <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
+                <div class="collapse mt-5" id="criadorDesafio">
+                  <div class="row form-group">
+                    <div class="col-md-12 mb-3 mb-md-0">
+                      <label class="text-black" for="nome">Nome</label>
+                      <input type="text" id="nome" name="nome" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-12">
+                      <label class="text-black" for="lname">E-mail</label>
+                      <input type="email" id="email" name="email" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-12 mb-3 mb-md-0">
+                      <label class="text-black" for="fname">CPF ou CNPJ</label>
+                      <input type="text" id="cpf" name="cpf" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-12 mb-3 mb-md-0">
+                      <label class="text-black" for="fname">CEP</label>
+                      <input type="text" id="cep" name="cep" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-6">
+                      <label class="text-black" for="email">Senha</label> 
+                      <input type="password" id="senha" name="senha" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                      <label class="text-black" for="subject">Confirmar senha</label> 
+                      <input type="password" id="confirmarSenha" name="confirmarSenha" class="form-control">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="" id="termos" name="termos" required>
+                      <label class="form-check-label" for="termos">
+                        Concordo com os <a href="">termos e condições</a>
+                      </label>
+                      <div class="invalid-feedback">
+                        Você deve concordar, antes de continuar.
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="row form-group">
+                    <div class="col-md-12">
+                      <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
+                    </div>
+                  </div>
                 </div>
               </div>
             </form>
