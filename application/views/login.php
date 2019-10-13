0a95c0346caa96dacdbfd37ee12c3f094a4b87ec
joao paulo <joaopaulopmedeiros@gmail.com>
2019-10-13 12:29:10 -0300

    cadastro form

--- application/views/login.php
+++ application/views/login.php
@@ -11,94 +11,80 @@
       </div>
     </div>  
 
-    
     <div class="site-section bg-light">
       <div class="container">
         <div class="row">
           <div class="col-md-6 mb-5">
-
-            <form action="<?=base_url()?>user_cidadao/cadastrar" class="p-5 bg-white">
+            <form class="p-5 bg-white">
              
               <h3>Cadastre-se</h3>
-              <p> Insira aqui seus dados para que possamos fazer o seu cadastro!</p>
-              <div class="row form-group">
-
-                <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="text-black" for="fname">Nome</label>
-                  <input type="text" id="fname" class="form-control">
-                </div>
-              </div>
-
-                <div class="row form-group">
-                
-                <div class="col-md-12">
-                  <label class="text-black" for="lname">E-mail</label>
-                  <input type="email" id="email" class="form-control">
-                </div>
-              </div>
-
-               <div class="row form-group">
-
-                <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="text-black" for="fname">CPF</label>
-                  <input type="text" id="" class="form-control">
-                </div>
-              </div>
-
-                <div class="row form-group">
-
-                <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="text-black" for="fname">CEP</label>
-                  <input type="text" id="" class="form-control">
-                </div>
-              </div>
-
-              <div class="row form-group">
-                
-                <div class="col-md-6">
-                  <label class="text-black" for="email">Senha</label> 
-                  <input type="password" id="" class="form-control">
-                </div>
-                 <div class="col-md-6">
-                  <label class="text-black" for="subject">Confirmar senha</label> 
-                  <input type="password" id="" class="form-control">
-                </div>
-              </div>
-
-              <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="usuario" id="usuario" value="usuario" checked>
-                  <label class="form-check-label" for="usuario">
-                    Cidadão
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="cooperativa" id="cooperativa" value="cooperativa">
-                  <label class="form-check-label" for="cooperativa">
-                    Cooperativa
-                  </label>
-                </div>
+              <p>Selecione o tipo de usuário: </p>
+              
+              <div class="btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-outline-primary mt-1"><input type="radio" id="btnCidadao" value="cidadao">Cidadão</label>
+                <label class="btn btn-outline-primary mt-1"><input type="radio" id="btnCriadorDesafio" value="criadorDesafio">Criador de Desafios</label>
               </div>
               
+              <div class="mt-5">
+                <div class="row form-group">
+                  <div class="col-md-12 mb-3 mb-md-0">
+                    <label class="text-black" for="nome">Nome</label>
+                    <input type="text" id="nome" name="nome" class="form-control">
+                  </div>
+                </div>
 
-              <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                  <label class="form-check-label" for="invalidCheck">
-                    Concordo com os <a href="">termos e condições</a>
-                  </label>
-                  <div class="invalid-feedback">
-                    Você deve concordar, antes de continuar.
+                <div class="row form-group">
+                  <div class="col-md-12">
+                    <label class="text-black" for="email">E-mail</label>
+                    <input type="email" id="email" name="email" class="form-control">
+                  </div>
+                </div>
+
+                <div class="row form-group">
+                  <div class="col-md-12 mb-3 mb-md-0">
+                    <label class="text-black" for="doc">Documento de identificação</label>
+                    <input type="text" id="doc" name="doc" class="form-control">
+                  </div>
+                </div>
+
+                <div class="row form-group">
+                  <div class="col-md-12 mb-3 mb-md-0">
+                    <label class="text-black" for="cep">CEP</label>
+                    <input type="text" id="cep" name="cep" class="form-control">
+                  </div>
+                </div>
+
+                <div class="row form-group">
+                  <div class="col-md-6">
+                    <label class="text-black" for="senha">Senha</label> 
+                    <input type="password" id="senha" name="senha" class="form-control">
+                  </div>
+                  <div class="col-md-6">
+                    <label class="text-black" for="confirmarSenha">Confirmar senha</label> 
+                    <input type="password" id="confirmarSenha" name="confirmarSenha" class="form-control">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="termos" name="termos" required>
+                    <label class="form-check-label" for="termos">
+                      Concordo com os <a href="">termos e condições</a>
+                    </label>
+                    <div class="invalid-feedback">
+                      Você deve concordar, antes de continuar.
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row form-group">
+                  <div class="col-md-12">
+                    <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
                   </div>
                 </div>
               </div>
-
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
-                </div>
-              </div>
             </form>
+
           </div>
 
           <div class="col-md-6">
@@ -107,18 +93,16 @@
               <h3>Login</h3>
 
               <div class="row form-group">
-                
                 <div class="col-md-12">
-                  <label class="text-black" for="lname">E-mail</label>
+                  <label class="text-black" for="email">E-mail</label>
                   <input type="email" id="email" class="form-control">
                 </div>
               </div>
 
               <div class="row form-group">
-                
                 <div class="col-md-12">
-                  <label class="text-black" for="email">Senha</label> 
-                  <input type="password" id="" class="form-control">
+                  <label class="text-black" for="senha">Senha</label> 
+                  <input type="password" id="senha" class="form-control">
                 </div>
               </div>
 
@@ -132,3 +116,21 @@
         </div>
       </div>
     </div>
+
+    <script type="text/javascript" language="javascript" >
+      $(document).ready(function() {
+
+        function fetch_data()
+        {
+          $.ajax({
+            url:"<?php echo base_url(); ?>",
+            method:"POST",
+            data:{data_action:'fetch_all'},
+            success:function(data)
+            {
+              $('tbody').html(data);
+            }
+          });
+        }
+      });
+    </script>
