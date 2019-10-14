557275dd3e9dfeedcea382d10bd3a19a47d40fa6
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-10-14 00:13:55 -0300

    jquery link fix

--- application/views/footer.php
+++ application/views/footer.php
@@ -49,17 +49,8 @@
     </footer>
   </div>
 
-  <script src="<?= base_url();?>assets/js/jquery-3.3.1.min.js"></script>
-  <script src="<?= base_url();?>assets/js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="<?= base_url();?>assets/js/jquery-ui.js"></script>
-  <script src="<?= base_url();?>assets/js/popper.min.js"></script>
-  <script src="<?= base_url();?>assets/js/bootstrap.min.js"></script>
-  <script src="<?= base_url();?>assets/js/owl.carousel.min.js"></script>
-  <script src="<?= base_url();?>assets/js/jquery.stellar.min.js"></script>
-  <script src="<?= base_url();?>assets/js/jquery.countdown.min.js"></script>
-  <script src="<?= base_url();?>assets/js/jquery.magnific-popup.min.js"></script>
-  <script src="<?= base_url();?>assets/js/bootstrap-datepicker.min.js"></script>
   <script src="<?= base_url();?>assets/js/aos.js"></script>
   <script src="<?= base_url();?>assets/js/main.js"></script>
-  </body>
+  
+</body>
 </html>
