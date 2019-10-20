2bc0495738d812fa93af79d6cd1ab1ffa4212148
joao paulo <joaopaulopmedeiros@gmail.com>
2019-10-20 09:57:18 -0300

    changing column from cpf to docCadastrado in cidadão table

--- sql/recicle-bd.sql
+++ sql/recicle-bd.sql
@@ -9,7 +9,7 @@ CREATE DATABASE IF NOT EXISTS recicle;
 
 -- Criacao de Tabela Cidadao, atributos, engine para transacao de dados e colecao de caracteres default
 CREATE TABLE IF NOT EXISTS recicle.cidadao(
-	cpf varchar(20) NOT NULL, -- pessoa física (cpf)
+	docCadastrado varchar(20) NOT NULL, -- pessoa física (cpf)
     login varchar (50) NOT NULL,
     senha varchar (20) NOT NULL,
     nome varchar (50) NOT NULL,
