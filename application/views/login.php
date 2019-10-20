efa8f5eebf98c1692b1d4fd0e4815c4ae34a8eba
joao paulo <joaopaulopmedeiros@gmail.com>
2019-10-20 09:45:54 -0300

    refactoring code to log user at first time by using AJAX

--- application/views/login.php
+++ application/views/login.php
@@ -119,53 +119,5 @@
       </div>
     </div>
 
-    <script type="text/javascript" language="javascript">
-      function idCidadao() {
-        document.getElementById('lblDoc').innerHTML = 'CPF';
-      }
-
-      function idCriadorDesafio() {
-        document.getElementById('lblDoc').innerHTML = 'CPF ou CNPJ';
-      }
-
-      $(document).ready(function(){
-        $(document).on('submit', '#user_form', function(event){
-          event.preventDefault();
-
-          var url = "";
-          if ($('#btnCidadao').is(':checked')) {
-            url = "http://localhost/recicle/user_cidadao/cadastrar";
-          }
-          else if ($('#btnCriadorDesafio').is(":checked")) {
-            url = "http://localhost/recicle/user_criadordesafio/cadastrar";
-          }
-
-          var dataUser = {
-            nome : $('#nome').val(),
-            login : $('#email').val(),
-            doc : $('#doc').val(),
-            cep : $('#cep').val(),
-            senha : $('#senha').val()
-          };
-
-          $.ajax({
-            url:url,
-            method:"POST",
-            data:dataUser,
-            dataType:"json",
-            success:function(data)
-            {
-              if(data.success)
-              {
-                console.log("deu certo :D");
-              }
-
-              if(data.error)
-              {
-                console.log("deu errado :(");
-              }
-            }
-          })
-        });
-      });
+    <script src="<?= base_url();?>assets/js/ajax.js"> type="text/javascript" language="javascript">
     </script>
---
+++ assets/js/ajax.js
@@ -0,0 +1,48 @@
+function idCidadao() {
+    document.getElementById('lblDoc').innerHTML = 'CPF';
+}
+
+function idCriadorDesafio() {
+    document.getElementById('lblDoc').innerHTML = 'CPF ou CNPJ';
+}
+
+$(document).ready(function(){
+    $(document).on('submit', '#user_form', function(event){
+      event.preventDefault();
+
+      var url = "";
+      if ($('#btnCidadao').is(':checked')) {
+        url = "http://localhost/recicle/user_cidadao/cadastrar";
+      }
+      else if ($('#btnCriadorDesafio').is(":checked")) {
+        url = "http://localhost/recicle/user_criadordesafio/cadastrar";
+      }
+
+      var dataUser = {
+        nome : $('#nome').val(),
+        login : $('#email').val(),
+        doc : $('#doc').val(),
+        cep : $('#cep').val(),
+        senha : $('#senha').val()
+      };
+
+       $.ajax({
+            url:url,
+            method:"POST",
+            data:dataUser,
+            dataType:"json",
+            success:function(data)
+            {
+              if(data.success)
+              {
+                console.log("deu certo :D");
+              }
+
+              if(data.error)
+              {
+                console.log("deu errado :(");
+              }
+            }
+        })
+    });
+});
