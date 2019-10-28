98f2001536123bcb85d0ed4fc9efda1477691e74
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-10-28 19:57:19 -0300

    adding encryption key

--- application/config/config.php
+++ application/config/config.php
@@ -325,7 +325,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '46a79a8fd9fb015b70d549899fc2ac61';
 
 /*
 |--------------------------------------------------------------------------
