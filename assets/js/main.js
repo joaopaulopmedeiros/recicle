87dd8201c4ec78091571820c02d83d7ab6ac58e9
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-03 20:42:03 -0300

    changing menu function; added function to preview challenge image

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 		var titleWindow = document.getElementsByTagName("TITLE")[0].text;
 		var titulo;
 
-		if (titleWindow == 'Ecopontos' || titleWindow == 'Minha Conta' || titleWindow == 'Acesso restrito') {
+		if (titleWindow == 'Ecopontos' || titleWindow == 'Minha Conta' || titleWindow == 'Acesso restrito' || titleWindow == 'Novo Desafio') {
 			titulo = true;
 		}
 		else {
@@ -262,4 +262,23 @@ jQuery(document).ready(function ($) {
 	siteDatePicker();
 
 });
+
+function previewImage()
+{
+    const $ = document.querySelector.bind(document);
+
+    const previewImg = $('.preview-img');
+    const fileChooser = $('.file-chooser');
+    const fileButton = $('.file-button');
+
+    fileButton.onclick = () => fileChooser.click();
+
+    fileChooser.onchange = e => {
+        const fileToUpload = e.target.files.item(0);
+        const reader = new FileReader();
+        reader.onload = e => previewImg.src = e.target.result;
+        reader.readAsDataURL(fileToUpload);
+    };
+}
+
 
