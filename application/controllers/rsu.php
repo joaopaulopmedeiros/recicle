df4999444839bb7e9e578e75e56c8e3c6b80c4e9
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-11-04 11:12:12 -0200

    adding insert method

--- application/controllers/rsu.php
+++ application/controllers/rsu.php
@@ -29,6 +29,19 @@ class RSU extends CI_Controller {
 
     public function insert()
     {
-        
+        $api_url = "http://localhost/recicle-api/rsu/insert";
+
+        $form_data = array(
+			'tipo' => $this->input->post('tipo')
+	    );
+  
+        $client = curl_init($api_url);
+        curl_setopt($client, CURLOPT_POST, true);
+        curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($client);
+        curl_close($client);
+    
+        echo $response;
     }
 }
