9d10d20c35f695960611cb7d6f3487c53b99eb40
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-04 00:36:24 -0300

    alteração no método de exibição dos desafios; adição de método para inserir desafios

--- application/controllers/desafios.php
+++ application/controllers/desafios.php
@@ -5,39 +5,79 @@ class Desafios extends CI_Controller {
 
 	public function index() 
 	{
-	    $api_url = "http://localhost/recicle-api/desafios/index";
-  
-        $client = curl_init($api_url);
-        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($client);
-        curl_close($client);
-        $result = json_decode($response);
+        if ($this->input->post('data_action')) {
+            $data_action = $this->input->post('data_action');
+            $api_url = "";
 
-        $output = '';
+            if ($data_action == "all")
+            {
+                $api_url = "http://localhost/recicle-api/desafios/exibirTodosOsDesafios";
+            }
+            elseif($data_action == "meus-desafios")
+            {
+                $api_url = "http://localhost/recicle-api/desafios/exibirMeusDesafios";
+            }
 
-        if(count($result) > 0)
-        {
-            foreach($result as $row)
+            $data = array(
+                'user_id' => $this->input->post('user_id')
+            );
+    
+            $client = curl_init($api_url);
+            curl_setopt($client, CURLOPT_POST, true);
+            curl_setopt($client, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($client);
+            curl_close($client);
+            $result = json_decode($response);
+
+            $output = '';
+
+            if(count($result) > 0)
+            {
+                foreach($result as $row)
+                {
+                    $output .= '
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                        <div class="h-entry">
+                            <img src="'.base_url().'assets/images/desafio.png" alt="Image" class="img-fluid">
+                            <a href="#" class="h4"><div class="text-primary">'.$row->titulo.'</div></a>
+                            <div class="meta mb-4">'.$row->nome.'<span class="mx-2">&bullet;</span>'.$row->tipo.'</div>
+                        </div> 
+                    </div>
+                    ';
+                }
+            }
+            else
             {
                 $output .= '
-                <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
-                    <div class="h-entry">
-                        <img src="'.base_url().'assets/images/blog_2.jpg" alt="Image" class="img-fluid">
-                        <a href="#" class="font-size-regular"><div class="text-primary">'.$row->titulo.'</div></a>
-                        <div class="meta mb-4">'.$row->nome.'<span class="mx-2">&bullet;</span>'.$row->tipo.'<span class="mx-2">&bullet;</span>Jan 18, 2019</div>
-                    </div> 
+                <div class="col-12" role="alert">
+                    <h4 class="text-center">Ainda não há desafios cadastrados :(</h4>
                 </div>
                 ';
             }
+            echo $output;
         }
         else
         {
-            $output .= '
-            <div class="col-12" role="alert">
-                <h4 class="text-center">Ainda não há desafios cadastrados :(</h4>
-            </div>
-            ';
+            redirect('user_criadordesafio/index');
         }
-        echo $output;
+    }
+
+    public function insert()
+    {
+        $api_url = "http://localhost/recicle-api/desafios/inserir";
+      
+        $form_data = array(
+            
+        );
+      
+        $client = curl_init($api_url);
+        curl_setopt($client, CURLOPT_POST, true);
+        curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($client);
+        curl_close($client);
+      
+        echo $response;
     }
 }
