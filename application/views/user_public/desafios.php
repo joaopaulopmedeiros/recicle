45ff2f4fc77369cb55ab67e1222628ce956dd6d6
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-04 00:37:40 -0300

    adição de data_action

--- application/views/user_public/desafios.php
+++ application/views/user_public/desafios.php
@@ -107,6 +107,7 @@
           $.ajax({
             url:"http://localhost/recicle/desafios/index",
             method:"POST",
+            data:{data_action:"all"},
             success:function(data)
             {
               $('#desafios').html(data);
