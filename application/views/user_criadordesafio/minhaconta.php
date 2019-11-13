93eaa127451081a0cbd4516961a18386ba68ffc2
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-11-13 11:45:57 -0300

    changing page content to display account information

--- application/views/user_criadordesafio/minhaconta.php
+++ application/views/user_criadordesafio/minhaconta.php
@@ -1,52 +1,19 @@
-<div class="site-section">
+    <div class="site-section">
       <div class="container mt-5">
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center border-primary">
-            <h2 class="font-weight-light text-primary">Editar conta</h2>
+            <h2 class="font-weight-light text-primary">Minha conta</h2>
           </div>
         </div>
 
-        <form action="#" class="p-5 bg-white">
-          <div class="row form-group">
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="nome">Nome</label>
-              <input type="text" id="nome" class="form-control">
-            </div>
+        <div class="row justify-content-around mb-5">
+          <div class="col-md-3">
+            <img src="<?= base_url()?>assets/images/user_icon.png" class="img-fluid border-rounded" alt="Imagem do perfil do usuário criador de desafios">
+          </div>
 
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="email">E-mail</label>
-              <input type="email" id="email" class="form-control">
-            </div>
+          <div class="col-md-7">
+            
           </div>
-        
-          <div class="row form-group">
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="cpf">CPF</label>
-              <input type="text" id="cpf" class="form-control">
-            </div>
-
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="cep">CEP</label>
-              <input type="text" id="cep" class="form-control">
-            </div>
-          </div>
-        
-          <div class="row form-group">
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="senha">Senha</label>
-              <input type="password" id="senha" class="form-control">
-            </div>
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="conf-senha">Confirmar senha</label>
-              <input type="password" id="conf-senha" class="form-control">
-            </div>
-          </div>
-        
-          <div class="row form-group">
-            <div class="col-md-12">
-              <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
