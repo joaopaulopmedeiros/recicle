dc4a026b1534fce2e151512bf34fcac47e6a9c18
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-16 01:56:53 -0300

    adding form to edit account

--- application/views/user_criadordesafio/minhaconta.php
+++ application/views/user_criadordesafio/minhaconta.php
@@ -7,12 +7,48 @@
         </div>
 
         <div class="row justify-content-around mb-5">
-          <div class="col-md-3">
-            <img src="<?= base_url()?>assets/images/user_icon.png" class="img-fluid border-rounded" alt="Imagem do perfil do usuário criador de desafios">
-          </div>
+          <div class="col-10">
+            <div id="editar_conta">
+              <div id="alerta">
 
-          <div class="col-md-7">
-            
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-6 mb-3">
+                  <label class="text-black" for="nome">Nome</label>
+                  <input type="text" id="nome" name="nome" class="form-control" readonly>
+                </div>
+
+                <div class="col-md-6">
+                  <label class="text-black" for="email">E-mail</label>
+                  <input type="email" id="email" name="email" class="form-control" readonly>
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-4 mb-3">
+                  <label class="text-black" for="doc" id="lblDoc">Documento de identificação</label>
+                  <input type="text" id="doc" name="doc" class="form-control" readonly>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                  <label class="text-black" for="cep">CEP</label>
+                  <input type="text" id="cep" name="cep" class="form-control" readonly>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                  <label class="text-black" for="senha">Senha</label> 
+                  <input type="password" id="senha" name="senha" value="aa" class="form-control" aria-describedby="passwordHelpBlock" readonly>
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <input type="submit" id="editar" onclick="editarForm()" value="Editar informações" class="btn btn-primary py-2 px-4 text-white">
+                  <input type="submit" id="cancelar" onclick="fecharForm()" value="Cancelar" class="btn btn-secondary py-2 px-4" hidden>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
