c7bb4001991279193c73f1a61fd85e33400fa221
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-16 21:13:34 -0300

    creating a controller for the 'criador de desafios'

---
+++ application/controllers/criador_desafio.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Criador_Desafio extends CI_Controller {
+
+	public function index() 
+	{
+        $api_url = "http://localhost/recicle-api/criadordesafio/fetch_single";
+
+        $data = array(
+            'docCadastrado' => $this->input->post('docCadastrado')
+        );
+
+        $client = curl_init($api_url);
+        
+        curl_setopt($client, CURLOPT_POST, true);
+        curl_setopt($client, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($client);
+        curl_close($client);
+
+        echo $response;
+    }
+
+    public function update()
+    {
+        $api_url = "http://localhost/recicle-api/criadordesafio/update";
+
+        $form_data = array(
+            'nome' => $this->input->post('nome'),
+            'login' => $this->input->post('login'),
+            'docCadastrado' => $this->input->post('docCadastrado'),
+            'cep' => $this->input->post('cep'),
+            'senha' => $this->input->post('senha')
+	    );
+  
+        $client = curl_init($api_url);
+        curl_setopt($client, CURLOPT_POST, true);
+        curl_setopt($client, CURLOPT_POSTFIELDS, $form_data);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($client);
+        curl_close($client);
+    
+        echo $response;
+    }
+}
