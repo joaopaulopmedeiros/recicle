0ecf93ed06e4e7728c18803af5c63b38bba8dba7
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-16 21:16:02 -0300

    create of exibirInformacoes  and ajax methods

---
+++ assets/js/ajax/editar_conta_criador.js
@@ -0,0 +1,57 @@
+function exibirInformacoes()
+{
+    $(document).ready(function(){
+        var data_user = {
+            docCadastrado : $('#user').val()
+        };
+
+        $.ajax({
+            url:"http://localhost/recicle/criador_desafio/index",
+            method:"POST",
+            data:data_user,
+            dataType:"json",
+            success:function(data)
+            {
+                $('#nome').val(data.nome);
+                $('#email').val(data.login);
+                $('#doc').val(data.docCadastrado);
+                $('#cep').val(data.cep);
+                $('#senha').val(data.senha);
+            }
+        });
+    });
+}
+exibirInformacoes();
+
+$(document).ready(function(){
+    $(document).on('submit', '#editar_conta', function(event){
+        event.preventDefault();
+
+        var dataUser = {
+            nome : $('#nome').val(),
+            login : $('#email').val(),
+            docCadastrado : $('#doc').val(),
+            cep : $('#cep').val(),
+            senha : $('#senha').val()
+        };
+
+        $.ajax({
+            url:"http://localhost/recicle/criador_desafio/update",
+            method:"POST",
+            data:dataUser,
+            dataType:"json",
+            success:function(data)
+            {
+                if(data.success)
+                {
+                    fecharForm();
+                    $('#alerta').html("<div class='alert alert-success' role='alert'>Os dados foram salvos com sucesso!</div>");
+                }
+                if(data.error)
+                {
+                    $('#alerta').html("<div class='alert alert-danger' role='alert'>" + data['msg_erro'] + "</div>");
+                }
+            }
+        })
+    });
+});
