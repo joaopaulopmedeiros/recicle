2140e7ff66a6dd96d1f81cc26a6763b61cbf5e41
joao paulo <joaopaulopmedeiros@gmail.com>
2019-11-17 01:10:21 -0300

    two request for accepting challenge (client-side)

--- assets/js/ajax/aceitar_desafio.js
+++ assets/js/ajax/aceitar_desafio.js
@@ -1,11 +1,30 @@
 function adicionarDesafio(){
+    
+    var url = window.location.href;
+    var idx = url.indexOf("desafio");
+    var desafio = url.substring(idx).split("/")[1];
+
+    var url2 = window.location.href;
+    var idx2 = url2.indexOf("recicle");
+    var tipo_user = url2.substring(idx2).split("/")[1];
+
     $.ajax({
         url:"http://localhost/recicle/desafios/ver_desafio",
         method:"POST",
-        data:{x},
-        success:function(data)
-        {
-         $('#desafio').html(data);
+        data:{id_desafio:desafio, user: tipo_user},
+        dataType: "json",
+        success:function(data){
+            console.log("sucesso");
+        }
+    });
+
+    $.ajax({
+        url:"http://localhost/recicle/desafios/aceitar_desafio",
+        method:"POST",
+        data: {idCriadorDesafio,id_user,idTipoRSU,idTipoBonificacao,idDesafio},
+        dataType: "json",
+        success:function(data){
+            console.log("sucesso");
         }
     });
 }
