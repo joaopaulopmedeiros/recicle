b9be8a1210ddd900d6b4770baf56d379382dfc5c
joao paulo <joaopaulopmedeiros@gmail.com>
2019-11-17 01:11:09 -0300

    getting user_id by session in JS

--- application/views/ver_desafio.php
+++ application/views/ver_desafio.php
@@ -3,7 +3,10 @@
         
       </div>
     </div>
-
+    
+    <script type="text/javascript" language="javascript">
+      var id_user = "<?php echo $this->session->cidadao['doc']?>";
+    </script>
     <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/ver_desafio.js"></script>
     <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/aceitar_desafio.js"></script>
     
