64d239b1b196c3d0424b6e76b678f2cfc28bacb9
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-18 00:38:59 -0300

    adding form tags

--- application/views/user_cidadao/minhaconta.php
+++ application/views/user_cidadao/minhaconta.php
@@ -1,52 +1,63 @@
-<div class="site-section">
+    <div class="site-section">
       <div class="container mt-5">
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center border-primary">
-            <h2 class="font-weight-light text-primary">Editar conta</h2>
+            <h2 class="font-weight-light text-primary">Minha conta</h2>
           </div>
         </div>
 
-        <form action="#" class="p-5 bg-white">
-          <div class="row form-group">
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="nome">Nome</label>
-              <input type="text" id="nome" class="form-control">
-            </div>
+        <div class="row justify-content-around mb-5">
+          <div class="col-10">
+            <form id="editar_conta">
+              <div id="alerta">
 
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="email">E-mail</label>
-              <input type="email" id="email" class="form-control">
-            </div>
-          </div>
-        
-          <div class="row form-group">
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="cpf">CPF</label>
-              <input type="text" id="cpf" class="form-control">
-            </div>
+              </div>
 
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="cep">CEP</label>
-              <input type="text" id="cep" class="form-control">
-            </div>
+              <div class="row form-group">
+                <div class="col-md-6 mb-3">
+                  <label class="text-black" for="nome">Nome</label>
+                  <input type="text" id="nome" name="nome" class="form-control" readonly>
+                </div>
+
+                <div class="col-md-6">
+                  <label class="text-black" for="email">E-mail</label>
+                  <input type="email" id="email" name="email" class="form-control" readonly>
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-4 mb-3">
+                  <label class="text-black" for="doc" id="lblDoc">CPF</label>
+                  <input type="text" id="doc" name="doc" class="form-control" readonly>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                  <label class="text-black" for="cep">CEP</label>
+                  <input type="text" id="cep" name="cep" class="form-control" readonly>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                  <label class="text-black" for="senha">Senha</label> 
+                  <input type="password" id="senha" name="senha" class="form-control" aria-describedby="passwordHelpBlock" readonly>
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <input type="button" id="editar" onclick="editarForm()" value="Editar informações" class="btn btn-primary py-2 px-4 text-white">
+                  <input type="submit" id="salvar" value="Salvar alterações" class="btn btn-primary py-2 px-4 text-white" hidden>
+                  <input type="button" id="cancelar" onclick="fecharForm()" value="Cancelar" class="btn btn-secondary py-2 px-4" hidden>
+                </div>
+              </div>
+            </form>
           </div>
-        
-          <div class="row form-group">
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="senha">Senha</label>
-              <input type="password" id="senha" class="form-control">
-            </div>
-            <div class="col-md-6 mb-3 mb-md-0">
-              <label class="text-black" for="conf-senha">Confirmar senha</label>
-              <input type="password" id="conf-senha" class="form-control">
-            </div>
-          </div>
-        
-          <div class="row form-group">
-            <div class="col-md-12">
-              <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
+
+    <script>
+      var id_user = "<?php echo $this->session->cidadao['doc']?>";
+      var email_user = "<?php echo $this->session->cidadao['login']?>";
+    </script>
+
+    <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/editar_conta_cidadao.js"></script>
