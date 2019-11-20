0fe36859e718a7cfcf67327948f0551683ae31bb
joao paulo <joaopaulopmedeiros@gmail.com>
2019-11-20 17:20:03 -0300

    file altered intending to specify what type of user is requesting to see the challenge (user_criadordesafio)

--- assets/js/ajax/meus_desafios.js
+++ assets/js/ajax/meus_desafios.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
     function exibirDesafios(){
-        var user = id_user;
-        
+
+    var url = window.location.href;
+    var idx = url.indexOf("recicle");
+    var user = url.substring(idx).split("/")[1]; //tipo de usuario = user_criadordesafio
+    var data_action = "meus-desafios";
+
         $.ajax({
             url:"http://localhost/recicle/desafios/index",
             method:"POST",
-            data:{data_action:"meus-desafios", user_id:user},
+            data:{data_action, user_id, user},
             success:function(data)
             {
             $('#desafios').html(data);
             }
         });
     }
+
     exibirDesafios();
 });
