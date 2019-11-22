67754cfa73106a228b33f7033481fc26e58c73ee
joao paulo <joaopaulopmedeiros@gmail.com>
2019-11-22 00:03:41 -0300

    viewing info of a challenge as user_public

---
+++ application/views/user_public/ver_desafio.php
@@ -0,0 +1,7 @@
+<div class="site-section">
+      <div class="container" id="desafio">
+        
+      </div>
+</div>
+<script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/ver_desafio.js"></script>
+    
