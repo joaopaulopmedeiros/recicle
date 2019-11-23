b355c94a38c6e3070f9300687e457b0ddcc217c6
joao paulo <joaopaulopmedeiros@gmail.com>
2019-11-22 21:24:02 -0300

    getting data from filter - no refresh page

--- assets/js/ajax/filtro.js
+++ assets/js/ajax/filtro.js
@@ -26,5 +26,53 @@ $(document).ready(function(){
         }
       });
     }
-    exibirDadosCamposFiltro();
-  });
+    
+    function enviar(){
+    
+      $(document).submit(function(){
+
+        var dataFiltro = {
+          idTipoRSU : $('#lixo').val(),
+          idTipoBonificacao : $('#bonificacao').val(),
+          idCriadorDesafio : $('#ecoponto').val()
+        };
+        
+        console.log(dataFiltro);
+
+        return false;
+      });
+      
+      
+    }
+
+   exibirDadosCamposFiltro();
+   enviar();
+    
+   /*$(document).on('submit', '#filtro', function(event)
+   { 
+      event.preventDefault();
+
+      var dataFiltro = {
+        idTipoRSU : $('#lixo').val(),
+        idTipoBonificacao : $('#bonificacao').val(),
+        idCriadorDesafio : $('#ecoponto').val(),
+      };
+      $('#filtro')[0].reset();
+      
+      $.ajax({
+        url:"http://localhost/recicle/filtro/index",
+        method:"POST",
+        data:dataFiltro,
+        dataType:"json",
+        success:function(data)
+        {
+          $('#filtro')[0].reset();
+          exibirDadosCamposFiltro();
+          $('#alert').html("<div class='alert alert-success alert-dismissible fade show' role='alert'>filtrado com sucesso!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>");
+          
+        }
+      })
+ 
+   });*/
+
+});
