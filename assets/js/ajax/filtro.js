a11551efad452b6315a6283ee80fb63e70e989b0
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-24 00:40:38 -0300

    changing indentation; implementing filter by ajax

--- assets/js/ajax/filtro.js
+++ assets/js/ajax/filtro.js
@@ -1,78 +1,59 @@
 $(document).ready(function(){
-    function exibirDadosCamposFiltro()
-    {
-      $.ajax({
-        url:"http://localhost/recicle/rsu/index",
-        method:"POST",
-        success:function(data)
-        {
-          $('#lixo').html(data);
-        }
-      });
-      $.ajax({
-        url:"http://localhost/recicle/bonificacao/index",
-        method:"POST",
-        success:function(data)
-        {
-          $('#bonificacao').html(data);
-        }
-      });
-      $.ajax({
-        url:"http://localhost/recicle/criador_desafio/fetch_all",
-        method:"POST",
-        success:function(data)
-        {
-          $('#ecoponto').html(data);
-        }
-      });
-    }
+  function exibirDadosCamposFiltro()
+  {
+    $.ajax({
+      url:"http://localhost/recicle/rsu/index",
+      method:"POST",
+      success:function(data)
+      {
+        $('#lixo').html(data);
+      }
+    });
+    $.ajax({
+      url:"http://localhost/recicle/bonificacao/index",
+      method:"POST",
+      success:function(data)
+      {
+        $('#bonificacao').html(data);
+        $('#bonificacao').append('<option value="todos">Todos</option>');
+      }
+    });
+    $.ajax({
+      url:"http://localhost/recicle/criador_desafio/fetch_all",
+      method:"POST",
+      success:function(data)
+      {
+        $('#ecoponto').html(data);
+        $('#ecoponto').append('<option value="todos">Todos</option>');
+      }
+    });
+  }
+  
+  exibirDadosCamposFiltro();
+  
+
+  $(document).on('submit', '#filtro', function(event){ 
+    event.preventDefault();
+
+    var url = window.location.href;
+    var idx = url.indexOf("recicle");
+    var tipo_user = url.substring(idx).split("/")[1];
+
+    var dataFiltro = {
+      idTipoRSU : $('#lixo').val(),
+      idTipoBonificacao : $('#bonificacao').val(),
+      idCriadorDesafio : $('#ecoponto').val(),
+      user : tipo_user
+    };
     
-    function enviar(){
-    
-      $(document).submit(function(){
-
-        var dataFiltro = {
-          idTipoRSU : $('#lixo').val(),
-          idTipoBonificacao : $('#bonificacao').val(),
-          idCriadorDesafio : $('#ecoponto').val()
-        };
-        
-        console.log(dataFiltro);
-
-        return false;
-      });
-      
-      
-    }
-
-   exibirDadosCamposFiltro();
-   enviar();
-    
-   /*$(document).on('submit', '#filtro', function(event)
-   { 
-      event.preventDefault();
-
-      var dataFiltro = {
-        idTipoRSU : $('#lixo').val(),
-        idTipoBonificacao : $('#bonificacao').val(),
-        idCriadorDesafio : $('#ecoponto').val(),
-      };
-      $('#filtro')[0].reset();
-      
-      $.ajax({
-        url:"http://localhost/recicle/filtro/index",
-        method:"POST",
-        data:dataFiltro,
-        dataType:"json",
-        success:function(data)
-        {
-          $('#filtro')[0].reset();
-          exibirDadosCamposFiltro();
-          $('#alert').html("<div class='alert alert-success alert-dismissible fade show' role='alert'>filtrado com sucesso!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>");
-          
-        }
-      })
- 
-   });*/
-
+    $.ajax({
+      url:"http://localhost/recicle/filtro/index",
+      method:"POST",
+      data:dataFiltro,
+      success:function(data)
+      {
+        $('#desafios-filtrados').html(data);
+      }
+    });
+  });
 });
