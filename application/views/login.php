a225e990a4187a56ee3b0b78e186b8df3ed11eee
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-24 02:13:28 -0300

    fix links

--- application/views/login.php
+++ application/views/login.php
@@ -5,7 +5,7 @@
 
           <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
             <h1 class="text-white font-weight-light text-uppercase font-weight-bold">Login/Cadastro</h1>
-            <p class="breadcrumb-custom"><a href="index.html">Home</a> <span class="mx-2">&gt;</span> <span>Entrar</span></p>
+            <p class="breadcrumb-custom"><a href="<?= base_url()?>">Home</a> <span class="mx-2">&gt;</span> <span>Entrar</span></p>
           </div>
         </div>
       </div>
