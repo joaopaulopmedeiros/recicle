d1d6dcbf39df0ab7edfe2b3fea0ed8b052807ef9
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-24 02:29:03 -0300

    adding page 404

---
+++ application/views/error_404.php
@@ -0,0 +1,18 @@
+    <div class="site-section">
+      <div class="container">
+        <div class="row justify-content-around mt-5">
+          <div class="col-md-5 col-lg-5 align-self-center">
+            <img src="<?= base_url()?>assets/images/erro-404.png" alt="" class="img-fluid">
+          </div>
+
+          <div class="col-md-4 col-lg-4 align-self-center">
+            <div class="border-primary text-center">
+              <h2 class="font-weight-light text-primary pb-2">Página não encontrada</h2>
+            </div>
+            <div class="mt-4">
+              A página não foi encontrada. Faça <a href="<?= base_url()?>user_public/login">login</a> ou vá para a <a href="<?= base_url()?>">página inicial</a>.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
