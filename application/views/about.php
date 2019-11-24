9f6d65a5b3f6789f3fe26173b48e8b9848f433c0
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-24 02:06:51 -0300

    changing content

--- application/views/about.php
+++ application/views/about.php
@@ -1,8 +1,6 @@
     <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(<?php echo base_url();?>assets/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-
       <div class="container">
         <div class="row align-items-center justify-content-center text-center">
-
           <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
             <h1 class="text-white font-weight-light text-uppercase font-weight-bold">Sobre</h1>
             <p class="breadcrumb-custom"><a href="index.php">Home</a> <span class="mx-2">&gt;</span> <span>Sobre</span></p>
@@ -14,99 +12,75 @@
     <div class="site-section">
       <div class="container">
         <div class="row mb-5">
-          
           <div class="col-md-5 ml-auto mb-5 order-md-2" data-aos="fade">
             <img src="<?php echo base_url();?>assets/images/img_1.png" alt="Image" class="img-fluid rounded">
           </div>
+
           <div class="col-md-6 order-md-1" data-aos="fade">
-            <div class="text-left pb-1 border-primary mb-4">
+            <div class="text-center pb-1 border-primary mb-4">
               <h2 class="text-primary">Nossa história</h2>
             </div>
-            <p align = "justify">Somos alunos do curso de Informática para internet do Instituto Federal de Ciência e Tecnologia do Rio Grande do Norte, Campus Natal Zona Norte (RN). A qual o projeto Recicle foi desenvolvido sobre o tema de bioeconomia da semana nacional de Ciência e Tecnologia de 2019, com pretensão de obter nota para disciplina: Projeto integrador que ocorre no 3º ano do IFRN, sobre a orientação de Jaqueline Silva, Doutora em Redes. </p>
-            <p  align = "justify" class="mb-5">Com isso, partimos para o desenvolvimento do Software Recicle que inicialmente foi implementado na Zona Norte de Natal (RN), visando gerar Consciência ambiental para população e ao mesmo tempo econômica para região.</p>
+
+            <p class="text-justify">Somos alunos do curso de Informática para internet do Instituto Federal de Ciência e Tecnologia do Rio Grande do Norte, Campus Natal Zona Norte. O projeto Recicle foi desenvolvido sobre o tema de bioeconomia da Semana Nacional de Ciência e Tecnologia de 2019, com pretensão de obter nota para disciplina Desenvolvimento de Projeto Integrador  do 3º ano do IFRN, sobre a orientação de Jaqueline Silva, doutora em Redes.</p>
+            <p class="text-justify mb-5">Com isso, partimos para o desenvolvimento do Software Recicle que inicialmente foi implementado na Zona Norte de Natal (RN), visando gerar consciência ambiental para a população e ao mesmo tempo gerar economia para região.</p>
 
             <div class="row">
-              <div class="col-md-12 mb-md-5 mb-0 col-lg-6">
-                <div class="unit-4">
-                  <div class="unit-4-icon mb-3 mr-4"><i class="fa fa-home"></i></div>
-                  <div>
-                    <h3>Onde estamos?</h3>
-                    <p>IFRN Campus Natal Zona Norte Rua Brusque, 2926 - Potengi, Natal - RN, 59112-490. </p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-12 mb-md-5 mb-0 col-lg-6">
-                <div class="unit-4">
+              <div class="col-md-12 mb-0 col-lg-6">
+                <div>
+                  <h3 class="text-primary">Onde estamos?</h3>
+                  <p>IFRN Campus Natal Zona Norte Rua Brusque, 2926 - Potengi, Natal - RN, 59112-490. </p>
                 </div>
               </div>
             </div>
           </div>
-          
         </div>
       </div>
     </div>
 
     
 
-    <div class="site-section border-bottom">
+    <div class="site-section border-bottom" style="background-color:#F5F5F5">
       <div class="container">
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center border-primary">
             <h2 class="font-weight-light text-primary" data-aos="fade">Equipe desenvolvedora</h2>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+        <div class="row justify-content-around">
+          <div class="col-md-6 col-lg-2 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
             <div class="person">
               <img src="<?php echo base_url();?>assets/images/debora.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
               <h3>Débora Samara</h3>
-              <p class="position text-muted">System Analist - Responsável pela interação cliente-equipe</p>
-              <ul class="ul-social-circle">
-                <li><a href="#"><span class="icon-facebook"></span></a></li>
-                <li><a href="#"><span class="icon-twitter"></span></a></li>
-                <li><a href="#"><span class="icon-linkedin"></span></a></li>
-                <li><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+              <p class="position text-uppercase small">Responsável pela interação cliente-equipe</p>
             </div>
 
           </div>
-           <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+           <div class="col-md-6 col-lg-2 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
             <div class="person">
-              <img src="<?php echo base_url();?>assets/images/fabi.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
+              <img src="<?php echo base_url();?>assets/images/fabi.png" alt="Image" class="img-fluid rounded-circle mb-5">
               <h3>Fabiana Pereira</h3>
-              <p class="position text-muted">Web Designer - Experiência em desenvolvimento Front-End.</p>
-              <ul class="ul-social-circle">
-                <li><a href="#"><span class="icon-facebook"></span></a></li>
-                <li><a href="#"><span class="icon-twitter"></span></a></li>
-                <li><a href="#"><span class="icon-linkedin"></span></a></li>
-                <li><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+              <p class="position text-uppercase small">Design e programação web</p>
             </div>
           </div>
-          <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
+          <div class="col-md-6 col-lg-2 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
             <div class="person">
               <img src="<?php echo base_url();?>assets/images/igu.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
               <h3>Higor Nascimento</h3>
-              <p class="position text-muted">Web Designer - Experiência em design gráfico e web.</p>
-              <ul class="ul-social-circle">
-                <li><a href="#"><span class="icon-facebook"></span></a></li>
-                <li><a href="#"><span class="icon-twitter"></span></a></li>
-                <li><a href="#"><span class="icon-linkedin"></span></a></li>
-                <li><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+              <p class="position text-uppercase small">Design gráfico e web</p>
             </div>
           </div>
-          <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="300">
+          <div class="col-md-6 col-lg-2 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="300">
             <div class="person">
               <img src="<?php echo base_url();?>assets/images/jp.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
-              <h3>João Paulo</h3>
-              <p class="position text-muted">Web Programmer - Experiência em desenvolvimento Back-End.</p>
-              <ul class="ul-social-circle">
-                <li><a href="#"><span class="icon-facebook"></span></a></li>
-                <li><a href="#"><span class="icon-twitter"></span></a></li>
-                <li><a href="#"><span class="icon-linkedin"></span></a></li>
-                <li><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+              <h3>João Paulo Pereira</h3>
+              <p class="position text-uppercase small">Programação web</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-2 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="300">
+            <div class="person">
+              <img src="<?php echo base_url();?>assets/images/jaque.jpg" alt="Image" class="img-fluid rounded-circle mb-5">
+              <h3>Jaqueline Pereira</h3>
+              <p class="position text-uppercase small">Orientadora</p>
             </div>
           </div>
         </div>
