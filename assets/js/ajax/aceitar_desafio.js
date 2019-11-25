7da58617465580e308201c41d2dfbd10fe67a5e6
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-25 00:32:46 -0300

    adding cancelar_desafio function to js

--- assets/js/ajax/aceitar_desafio.js
+++ assets/js/ajax/aceitar_desafio.js
@@ -1,30 +1,31 @@
-function adicionarDesafio(){
-    
-    var url = window.location.href;
-    var idx = url.indexOf("desafio");
-    var desafio = url.substring(idx).split("/")[1];
-
-    var url2 = window.location.href;
-    var idx2 = url2.indexOf("recicle");
-    var tipo_user = url2.substring(idx2).split("/")[1];
-
-    $.ajax({
-        url:"http://localhost/recicle/desafios/ver_desafio",
-        method:"POST",
-        data:{id_desafio:desafio, user: tipo_user},
-        dataType: "json",
-        success:function(data){
-            console.log("sucesso");
-        }
+function adicionarDesafio()
+{
+    $(document).ready(function(){
+        $.ajax({
+            url:"http://localhost/recicle/desafios/aceitar_desafio",
+            method:"POST",
+            data: {idCriadorDesafio,id_user,idTipoRSU,idTipoBonificacao,idDesafio},
+            dataType:"JSON",
+            success:function(data){
+                $('#botao').html('<p class="mb-0"><a onclick="cancelarDesafio()" class="btn btn-danger py-3 px-5 text-white">Não quero participar</a></p>');
+            }
+        });
     });
+}
 
-    $.ajax({
-        url:"http://localhost/recicle/desafios/aceitar_desafio",
-        method:"POST",
-        data: {idCriadorDesafio,id_user,idTipoRSU,idTipoBonificacao,idDesafio},
-        dataType: "json",
-        success:function(data){
-            console.log("sucesso");
-        }
+function cancelarDesafio()
+{
+    $(document).ready(function(){
+        $.ajax({
+            url:"http://localhost/recicle/desafios/cancelar_desafio",
+            method:"POST",
+            data:{id_user,idDesafio},
+            dataType:"JSON",
+            success:function(data){
+                if (data.success) {
+                    $('#botao').html('<p class="mb-0"><a onclick="adicionarDesafio()" class="btn btn-primary py-3 px-5 text-white">Aceitar desafio</a></p>');
+                }
+            }
+        });
     });
 }
