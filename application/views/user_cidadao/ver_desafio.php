a820a555858492181c2510110ed656553e04082c
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-25 00:34:40 -0300

    fix js code

--- application/views/user_cidadao/ver_desafio.php
+++ application/views/user_cidadao/ver_desafio.php
@@ -3,12 +3,9 @@
         
       </div>
     </div>
-    <?php
-      echo 
-      '
-      <script>var id_user ='.$this->session->cidadao['doc'].'</script> 
-      ';
-    ?>
+
+    <script>var id_user = "<?php echo $this->session->cidadao['doc']?>";</script>
+
     <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/ver_desafio.js"></script>
     <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/aceitar_desafio.js"></script>
     
