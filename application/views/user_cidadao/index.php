4df49ea3e8a811cab15f1870936809dc27af0e9e
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-25 00:45:27 -0300

    correcting indentation, opening and closing of tags; content change

--- application/views/user_cidadao/index.php
+++ application/views/user_cidadao/index.php
@@ -1,4 +1,4 @@
-   <div class="site-blocks-cover overlay" style="background-image: url(<?php echo base_url();?>assets/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+    <div class="site-blocks-cover overlay" style="background-image: url(<?php echo base_url();?>assets/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
       <div class="container">
         <div class="row align-items-center justify-content-center text-center">
           <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
@@ -21,6 +21,7 @@
             <p>Cumpra desafios para ser premiado. Aqui você pode saber os desafios da semana e como participar.</p>
           </div>
         </div>
+
         <div class="col-md-4">
           <div class="feature-3 pricing h-100 text-center">
             <div class="icon">
@@ -32,6 +33,7 @@
             <p>Edite as configurações da sua conta do Recicle.</p>
           </div>
         </div>
+
         <div class="col-md-4">
           <div class="feature-1 pricing h-100 text-center">
             <div class="icon">
@@ -50,7 +52,6 @@
     
     <div class="site-section">
       <div class="container">
-
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center border-primary">
             <h2 class="font-weight-light text-primary">Bonificações</h2>
@@ -58,10 +59,9 @@
         </div>
 
         <div class="row justify-content-around">
-
           <div class="col-md-5 align-self-center">
             <div>
-              <p align = "justify">Você pode ganhar vários prêmios descartando de forma consciente! Cadastre já para participar dos desafios lançados pelas cooperativas e pontos de coleta próximas de você. Essas premiações podem ser  Gift Cards, dinheiro e descontos em faturas. </p>
+              <p class="text-justify">Você pode ganhar vários prêmios descartando de forma consciente! Essas premiações podem ser Gift Cards, dinheiro ou descontos em faturas.</p>
             </div>
           </div>
 
@@ -84,7 +84,6 @@
               </figure>
             </div>
 
-
             <div class="d-flex justify-content-center">
               <figure style="width:250px;">
                 <img src="<?php echo base_url();?>assets/images/spot.png" alt="Image">
@@ -96,12 +95,13 @@
                 <img src="<?php echo base_url();?>assets/images/uber.png" alt="Image">
               </figure>
             </div>
-
           </div>
         </div>
-        
       </div>
     </div>
+
+
+
     <div class="site-section" style="background-color: #F5F5F5;">
       <div class="container">
         <div class="row justify-content-center mb-5">
@@ -109,39 +109,33 @@
             <h2 class="font-weight-light text-primary">Lixos mais descartados</h2>
           </div>
         </div>
+      </div>
+
       <div class="container">
         <div class="row justify-content-center mb-5">
-          </div>
-        </div>
-        <div class="row">
           <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="" data-aos-delay="100">
             <div class="person">
-              <img src="<?php echo base_url();?>assets/images/imgLixo1.jpg" alt="Image" class="img-fluid mb-5">
+              <img src="<?php echo base_url();?>assets/images/imgLixo1.jpg" alt="Imagem representando resíduos eletrônicos" class="img-fluid mb-5">
             </div>
+          </div>
 
-          </div>
-           <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="" data-aos-delay="100">
+          <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="" data-aos-delay="100">
             <div class="person">
-              <img src="<?php echo base_url();?>assets/images/imgLixo2.jpg" alt="Image" class="img-fluid mb-5">
+              <img src="<?php echo base_url();?>assets/images/imgLixo2.jpg" alt="Imagem representando papel e plástico" class="img-fluid mb-5">
             </div>
           </div>
+
           <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="" data-aos-delay="200">
             <div class="person">
-              <img src="<?php echo base_url();?>assets/images/imgLixo4.jpg" alt="Image" class="img-fluid mb-5">
+              <img src="<?php echo base_url();?>assets/images/imgLixo4.jpg" alt="Imagem representando baterias e óleo" class="img-fluid mb-5">
             </div>
           </div>
           
           <div class="col-md-6 col-lg-3 mb-5 mb-lg-0" data-aos="" data-aos-delay="300">
             <div class="person">
-              <img src="<?php echo base_url();?>assets/images/imgLixo3.jpg" alt="Image" class="img-fluid mb-5">
+              <img src="<?php echo base_url();?>assets/images/imgLixo3.jpg" alt="Imagem representando alumínio e outros tipos de lixo" class="img-fluid mb-5">
             </div>
           </div>
-        </div>
-       </div>
-          </div>
-        </div>
-      </div>
-    </div>
         </div>
       </div>
     </div>
