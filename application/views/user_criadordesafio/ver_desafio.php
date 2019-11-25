7baf5b1e3037b4f26cfc47966457b7623a762cca
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-11-25 09:46:07 -0300

    changing page content

--- application/views/user_criadordesafio/ver_desafio.php
+++ application/views/user_criadordesafio/ver_desafio.php
@@ -1,24 +1,25 @@
-<div class="site-section">
-      <div class="container" id="desafio">
-            <table class="table">
-                    <thead>
-                        <th>id</th>
-                        <th>Desafio</th>
-                        <th>Cidadão</th>
-                        <th>Tipo de RSU</th>
-                        <th>Tipo de Bonificação</th>
-                        <th>Status</th>
-                    </thead>
-                    <tbody>
-                        <td id="#idDesafioAceito"></td>
-                        <td id="#desafio"></td>
-                        <td id="#cidadao"></td>
-                        <td id="#tipoRSU"></td>
-                        <td id="#bonificacao"></td>
-                        <td id="#cumprir"><button onclick="cumprirDesafio('.$row->idDesafioAceito.')" type="button" class="btn btn-success">Confirmar</button></td>       
-                    </tbody>
-            </table>
-      </div>
+<div class="site-section" style="background-color:#f5f5f5">
+    <div class="container">
+        <div id="desafio">
+        
+        </div>
+    </div>
 </div>
+
+<div class="site-section">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-7 col-10 mt-5 text-center border-primary">
+                <h2 class="font-weight-light text-primary">Usuários Participantes</h2>
+            </div>
+        </div>
+
+        <div id="tabela-desafio">
+        
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/ver_desafio.js"></script>
 <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/concorrentes_meus_desafios.js"></script>
 <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/cumprir_desafio.js"></script>
