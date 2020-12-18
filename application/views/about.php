e445ec088a26ccbd20a4885e8dfc3302106a30ed
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2020-12-17 21:46:50 -0300

    updating "sobre" page content

--- application/views/about.php
+++ application/views/about.php
@@ -11,26 +11,24 @@
 
     <div class="site-section">
       <div class="container">
-        <div class="row mb-5">
-          <div class="col-md-5 ml-auto mb-5 order-md-2" data-aos="fade">
-            <img src="<?php echo base_url();?>assets/images/img_1.png" alt="Image" class="img-fluid rounded">
-          </div>
-
-          <div class="col-md-6 order-md-1" data-aos="fade">
-            <div class="text-center pb-1 border-primary mb-4">
-              <h2 class="text-primary">Nossa história</h2>
-            </div>
-
-            <p class="text-justify">Somos alunos do curso de Informática para internet do Instituto Federal de Ciência e Tecnologia do Rio Grande do Norte, Campus Natal Zona Norte. O projeto Recicle foi desenvolvido sobre o tema de bioeconomia da Semana Nacional de Ciência e Tecnologia de 2019, com pretensão de obter nota para disciplina Desenvolvimento de Projeto Integrador  do 3º ano do IFRN, sobre a orientação de Jaqueline Silva, doutora em Redes.</p>
-            <p class="text-justify mb-5">Com isso, partimos para o desenvolvimento do Software Recicle que inicialmente foi implementado na Zona Norte de Natal (RN), visando gerar consciência ambiental para a população e ao mesmo tempo gerar economia para região.</p>
-
-            <div class="row">
-              <div class="col-md-12 mb-0 col-lg-6">
-                <div>
-                  <h3 class="text-primary">Onde estamos?</h3>
-                  <p>IFRN Campus Natal Zona Norte Rua Brusque, 2926 - Potengi, Natal - RN, 59112-490. </p>
-                </div>
+        <div class="row mb-3 justify-content-center">
+          <div class="col-11 col-md-9" data-aos="fade">
+            <div id="nossa-historia" class="mb-5">
+              <div class="text-center pb-1 border-primary mb-4">
+                <h2 class="text-primary">Nossa história</h2>
               </div>
+
+              <p class="text-justify">Somos alunos do curso de Informática para internet do Instituto Federal de Ciência e Tecnologia do Rio Grande do Norte, Campus Natal Zona Norte. O projeto Recicle foi desenvolvido sobre o tema de bioeconomia da Semana Nacional de Ciência e Tecnologia de 2019, com pretensão de obter nota para disciplina Desenvolvimento de Projeto Integrador  do 3º ano do IFRN, sobre a orientação de Jaqueline Silva, doutora em Redes.</p>
+              <p class="text-justify">Com isso, partimos para o desenvolvimento do Software Recicle que inicialmente foi implementado na Zona Norte de Natal (RN), visando gerar consciência ambiental para a população e ao mesmo tempo gerar economia para região.</p>
+            </div>
+            
+            <div id="sobre-recicle">
+              <div class="text-center pb-1 border-primary mb-4">
+                <h2 class="text-primary">Sobre o Recicle</h2>
+              </div>
+
+              <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
           </div>
         </div>
