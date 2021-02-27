eaca084c1a6813fcc91b9c5ab8f86e38f4473b66
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-02-27 19:02:33 -0300

    updating font to montserrat

--- application/views/user_cidadao/header.php
+++ application/views/user_cidadao/header.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="<?php echo base_url();?>assets/fonts/icomoon/style.css">
 
     <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/favicon.ico" type="image/x-icon">
