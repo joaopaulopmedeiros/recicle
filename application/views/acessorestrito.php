66c8d2c715d16e7b05d81b3ad4d5ae5c6b6f5c13
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-02-28 23:31:52 -0300

    fixing path image and changing title styles

--- application/views/acessorestrito.php
+++ application/views/acessorestrito.php
@@ -1,13 +1,13 @@
     <div class="site-section">
       <div class="container">
-        <div class="row justify-content-around mt-5">
+        <div class="row justify-content-around">
           <div class="col-md-5 col-lg-5 align-self-center">
-            <img src="<?= base_url()?>assets/images/acesso-negado.png" alt="" class="img-fluid">
+            <img src="<?= base_url()?>assets/images/illustrations/acesso-negado.png" class="img-fluid">
           </div>
 
           <div class="col-md-4 col-lg-4 align-self-center">
-            <div class="border-primary text-center">
-              <h2 class="font-weight-light text-primary pb-2">Acesso Negado</h2>
+            <div class="text-center text-uppercase">
+              <h2 class="title-section pb-2">Acesso Negado</h2>
             </div>
             <div class="mt-4">
               Você não possui permissão para acessar esta página. Faça <a href="<?= base_url()?>user_public/login">login</a> ou vá para a <a href="<?= base_url()?>">página inicial</a>.
