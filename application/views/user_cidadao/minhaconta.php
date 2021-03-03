9e60533e0d751a5090e0a5b60c4e17c3c9b94daa
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-03-03 20:08:42 -0300

    changing styles of my account page

--- application/views/user_cidadao/minhaconta.php
+++ application/views/user_cidadao/minhaconta.php
@@ -1,8 +1,8 @@
     <div class="site-section">
       <div class="container mt-5">
-        <div class="row justify-content-center mb-5">
-          <div class="col-md-7 text-center border-primary">
-            <h2 class="font-weight-light text-primary">Minha conta</h2>
+      <div class="row justify-content-center mb-5">
+          <div class="col-10 col-md-8 text-center text-uppercase">
+            <h2 class="title-section">Minha conta</h2>
           </div>
         </div>
 
@@ -15,38 +15,38 @@
 
               <div class="row form-group">
                 <div class="col-md-6 mb-3">
-                  <label class="text-black" for="nome">Nome</label>
+                  <label class="text-label" for="nome">Nome</label>
                   <input type="text" id="nome" name="nome" class="form-control" readonly>
                 </div>
 
                 <div class="col-md-6">
-                  <label class="text-black" for="email">E-mail</label>
+                  <label class="text-label" for="email">E-mail</label>
                   <input type="email" id="email" name="email" class="form-control" readonly>
                 </div>
               </div>
 
               <div class="row form-group">
                 <div class="col-md-4 mb-3">
-                  <label class="text-black" for="doc" id="lblDoc">CPF</label>
+                  <label class="text-label" for="doc" id="lblDoc">CPF</label>
                   <input type="text" id="doc" name="doc" class="form-control" readonly>
                 </div>
 
                 <div class="col-md-4 mb-3">
-                  <label class="text-black" for="cep">CEP</label>
+                  <label class="text-label" for="cep">CEP</label>
                   <input type="text" id="cep" name="cep" class="form-control" readonly>
                 </div>
 
                 <div class="col-md-4 mb-3">
-                  <label class="text-black" for="senha">Senha</label> 
+                  <label class="text-label" for="senha">Senha</label> 
                   <input type="password" id="senha" name="senha" class="form-control" aria-describedby="passwordHelpBlock" readonly>
                 </div>
               </div>
 
               <div class="row form-group">
                 <div class="col-md-12">
-                  <input type="button" id="editar" onclick="editarForm()" value="Editar informações" class="btn btn-primary py-2 px-4 text-white">
-                  <input type="submit" id="salvar" value="Salvar alterações" class="btn btn-primary py-2 px-4 text-white" hidden>
-                  <input type="button" id="cancelar" onclick="fecharForm()" value="Cancelar" class="btn btn-secondary py-2 px-4" hidden>
+                  <input type="button" id="editar" onclick="editarForm()" value="Editar informações" class="btn btn-green py-2 px-4">
+                  <input type="submit" id="salvar" value="Salvar alterações" class="btn btn-green py-2 px-4" hidden>
+                  <input type="button" id="cancelar" onclick="fecharForm()" value="Cancelar" class="btn btn-gray py-2 px-4" hidden>
                 </div>
               </div>
             </form>
