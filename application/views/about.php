9ea6d6b155a58326546f2ec7fbff3d08d50c31e7
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-03-04 11:28:06 -0300

    fixing text error

--- application/views/about.php
+++ application/views/about.php
@@ -1,71 +1,70 @@
-    <div class="site-section">
-      <div class="container">
-        <div class="row justify-content-center mb-5">
-          <div class="col-10 col-md-8 text-center text-uppercase" data-aos="fade">
-            <h2 class="title-section">Nossa história</h2>
-          </div>
-        </div>
-
-        <div class="row justify-content-center mb-2">
-          <div class="col-10 col-md-8" data-aos="fade">
-            <p class="text-justify">Somos alunos do curso de Informática para internet do Instituto Federal de Ciência e Tecnologia do Rio Grande do Norte, Campus Natal Zona Norte. O projeto Recicle foi desenvolvido sobre o tema de bioeconomia da Semana Nacional de Ciência e Tecnologia de 2019, com pretensão de obter nota para disciplina Desenvolvimento de Projeto Integrador  do 3º ano do IFRN, sobre a orientação de Jaqueline Silva, doutora em Redes.</p>
-            <p class="text-justify mb-5">Com isso, partimos para o desenvolvimento do Software Recicle que inicialmente foi implementado na Zona Norte de Natal (RN), visando gerar consciência ambiental para a população e ao mesmo tempo gerar economia para região.</p>
-          </div>
-        </div>
-
-        <div class="row justify-content-center">
-          <div class="col-10 col-md-8" data-aos="fade">
-            <h3 class="text-primary font-weight-bold mb-2">Onde estamos?</h3>
-            <div class="mb-1">IFRN Campus Natal Zona Norte</div>
-            <div>Rua Brusque, 2926 - divotengi, Natal - RN, 59112-490.</div>
-          </div>
-        </div>
+<div class="site-section">
+  <div class="container">
+    <div class="row justify-content-center mb-5">
+      <div class="col-10 col-md-8 text-center text-uppercase" data-aos="fade">
+        <h2 class="title-section">Nossa história</h2>
       </div>
     </div>
 
-    
+    <div class="row justify-content-center mb-2">
+      <div class="col-10 col-md-8" data-aos="fade">
+        <p class="text-justify">Somos alunos do curso de Informática para internet do Instituto Federal de Ciência e Tecnologia do Rio Grande do Norte, Campus Natal Zona Norte. O projeto Recicle foi desenvolvido sobre o tema de bioeconomia da Semana Nacional de Ciência e Tecnologia de 2019, com pretensão de obter nota para disciplina Desenvolvimento de Projeto Integrador  do 3º ano do IFRN, sobre a orientação de Jaqueline Silva, doutora em Redes.</p>
+        <p class="text-justify mb-5">Com isso, partimos para o desenvolvimento do Software Recicle que inicialmente foi implementado na Zona Norte de Natal (RN), visando gerar consciência ambiental para a população e ao mesmo tempo gerar economia para região.</p>
+      </div>
+    </div>
 
-    <div class="site-section">
-      <div class="container">
-        <div class="row justify-content-center mb-5">
-          <div class="col-10 col-md-8 text-center text-uppercase" data-aos="fade">
-            <h2 class="title-section">Equipe desenvolvedora</h2>
-          </div>
+    <div class="row justify-content-center">
+      <div class="col-10 col-md-8" data-aos="fade">
+        <h3 class="text-primary font-weight-bold mb-2">Onde estamos?</h3>
+        <div class="mb-1">IFRN Campus Natal Zona Norte</div>
+        <div>Rua Brusque, 2926 - Potengi, Natal - RN, 59112-490.</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="site-section">
+  <div class="container">
+    <div class="row justify-content-center mb-5">
+      <div class="col-10 col-md-8 text-center text-uppercase" data-aos="fade">
+        <h2 class="title-section">Equipe desenvolvedora</h2>
+      </div>
+    </div>
+
+    <div class="justify-content-around equipe">
+      <div class="person col-10 col-md-3 col-lg-2 mb-5 mb-md-0" data-aos="fade" data-aos-delay="100">
+        <img src="<?php echo base_url();?>assets/images/team/debora.jpg" alt="Débora Rodrigues" class="img-fluid rounded-circle mb-md-3">
+        <div class="info-person">
+          <h4>Débora Rodrigues</h4>
+          <p class="small">Interação cliente-equipe</p>
         </div>
+      </div>
 
-        <div class="justify-content-around equipe">
-          <div class="person col-10 col-md-3 col-lg-2 mb-5 mb-md-0" data-aos="fade" data-aos-delay="100">
-            <img src="<?php echo base_url();?>assets/images/team/debora.jpg" alt="Débora Rodrigues" class="img-fluid rounded-circle mb-md-3">
-            <div class="info-person">
-              <h4>Débora Rodrigues</h4>
-              <p class="small">Interação cliente-equipe</p>
-            </div>
-          </div>
+      <div class="person col-10 col-md-3 col-lg-2 mb-5 mb-md-0" data-aos="fade" data-aos-delay="100">
+        <img src="<?php echo base_url();?>assets/images/team/fabiana.png" alt="Fabiana Pereira" class="img-fluid rounded-circle mb-md-3">
+        <div class="info-person">
+          <h4>Fabiana Pereira</h4>
+          <p class="small">Programação web</p>
+        </div>
+      </div>
 
-          <div class="person col-10 col-md-3 col-lg-2 mb-5 mb-md-0" data-aos="fade" data-aos-delay="100">
-            <img src="<?php echo base_url();?>assets/images/team/fabiana.png" alt="Fabiana Pereira" class="img-fluid rounded-circle mb-md-3">
-            <div class="info-person">
-              <h4>Fabiana Pereira</h4>
-              <p class="small">Programação web</p>
-            </div>
-          </div>
+      <div class="person col-10 col-md-3 col-lg-2 mb-5 mb-md-0" data-aos="fade" data-aos-delay="200">
+        <img src="<?php echo base_url();?>assets/images/team/higor.jpg" alt="Higor Nascimento" class="img-fluid rounded-circle mb-md-3">
+        <div class="info-person">
+          <h4>Higor Nascimento</h4>
+          <p class="small">Design gráfico</p>
+        </div>
+      </div>
 
-          <div class="person col-10 col-md-3 col-lg-2 mb-5 mb-md-0" data-aos="fade" data-aos-delay="200">
-            <img src="<?php echo base_url();?>assets/images/team/higor.jpg" alt="Higor Nascimento" class="img-fluid rounded-circle mb-md-3">
-            <div class="info-person">
-              <h4>Higor Nascimento</h4>
-              <p class="small">Design gráfico</p>
-            </div>
-          </div>
-
-          <div class="person col-10 col-md-3 col-lg-2 mb-5 mb-md-0" data-aos="fade" data-aos-delay="300">
-            <img src="<?php echo base_url();?>assets/images/team/jp.jpg" alt="João Paulo Pereira" class="img-fluid rounded-circle mb-md-3">
-            <div class="info-person">
-              <h4>João Paulo Pereira</h4>
-              <p class="small">Programação web</p>
-            </div>
-          </div>
+      <div class="person col-10 col-md-3 col-lg-2 mb-5 mb-md-0" data-aos="fade" data-aos-delay="300">
+        <img src="<?php echo base_url();?>assets/images/team/jp.jpg" alt="João Paulo Pereira" class="img-fluid rounded-circle mb-md-3">
+        <div class="info-person">
+          <h4>João Paulo Pereira</h4>
+          <p class="small">Programação web</p>
         </div>
       </div>
     </div>
-    
+  </div>
+</div>
