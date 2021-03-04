906e6dbeeefd9d07754fee77b1bc7184d8fb3986
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-03-03 23:18:21 -0300

    fixing my account page

--- application/views/user_cidadao/minhaconta.php
+++ application/views/user_cidadao/minhaconta.php
@@ -1,63 +1,63 @@
-    <div class="site-section">
-      <div class="container mt-5">
-      <div class="row justify-content-center mb-5">
-          <div class="col-10 col-md-8 text-center text-uppercase">
-            <h2 class="title-section">Minha conta</h2>
-          </div>
-        </div>
-
-        <div class="row justify-content-around mb-5">
-          <div class="col-10">
-            <form id="editar_conta">
-              <div id="alerta">
-
-              </div>
-
-              <div class="row form-group">
-                <div class="col-md-6 mb-3">
-                  <label class="text-label" for="nome">Nome</label>
-                  <input type="text" id="nome" name="nome" class="form-control" readonly>
-                </div>
-
-                <div class="col-md-6">
-                  <label class="text-label" for="email">E-mail</label>
-                  <input type="email" id="email" name="email" class="form-control" readonly>
-                </div>
-              </div>
-
-              <div class="row form-group">
-                <div class="col-md-4 mb-3">
-                  <label class="text-label" for="doc" id="lblDoc">CPF</label>
-                  <input type="text" id="doc" name="doc" class="form-control" readonly>
-                </div>
-
-                <div class="col-md-4 mb-3">
-                  <label class="text-label" for="cep">CEP</label>
-                  <input type="text" id="cep" name="cep" class="form-control" readonly>
-                </div>
-
-                <div class="col-md-4 mb-3">
-                  <label class="text-label" for="senha">Senha</label> 
-                  <input type="password" id="senha" name="senha" class="form-control" aria-describedby="passwordHelpBlock" readonly>
-                </div>
-              </div>
-
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <input type="button" id="editar" onclick="editarForm()" value="Editar informações" class="btn btn-green py-2 px-4">
-                  <input type="submit" id="salvar" value="Salvar alterações" class="btn btn-green py-2 px-4" hidden>
-                  <input type="button" id="cancelar" onclick="fecharForm()" value="Cancelar" class="btn btn-gray py-2 px-4" hidden>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
+<div class="site-section">
+  <div class="container">
+    <div class="row justify-content-center mb-5">
+      <div class="col-10 col-md-8 text-center text-uppercase">
+        <h2 class="title-section">Minha conta</h2>
       </div>
     </div>
 
-    <script>
-      var id_user = "<?php echo $this->session->cidadao['doc']?>";
-      var email_user = "<?php echo $this->session->cidadao['login']?>";
-    </script>
+    <div class="row justify-content-around mb-5">
+      <div class="col-10">
+        <form id="editar_conta">
+          <div id="alerta">
 
-    <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/editar_conta_cidadao.js"></script>
+          </div>
+
+          <div class="row form-group">
+            <div class="col-md-6 mb-3">
+              <label class="text-label" for="nome">Nome</label>
+              <input type="text" id="nome" name="nome" class="form-control" readonly>
+            </div>
+
+            <div class="col-md-6">
+              <label class="text-label" for="email">E-mail</label>
+              <input type="email" id="email" name="email" class="form-control" readonly>
+            </div>
+          </div>
+
+          <div class="row form-group">
+            <div class="col-md-4 mb-3">
+              <label class="text-label" for="doc" id="lblDoc">CPF</label>
+              <input type="text" id="doc" name="doc" class="form-control" readonly>
+            </div>
+
+            <div class="col-md-4 mb-3">
+              <label class="text-label" for="cep">CEP</label>
+              <input type="text" id="cep" name="cep" class="form-control" readonly>
+            </div>
+
+            <div class="col-md-4 mb-3">
+              <label class="text-label" for="senha">Senha</label> 
+              <input type="password" id="senha" name="senha" class="form-control" aria-describedby="passwordHelpBlock" readonly>
+            </div>
+          </div>
+
+          <div class="row form-group">
+            <div class="col-md-12">
+              <input type="button" id="editar" onclick="editarForm()" value="Editar informações" class="btn btn-green py-2 px-4">
+              <input type="submit" id="salvar" value="Salvar alterações" class="btn btn-green py-2 px-4" hidden>
+              <input type="button" id="cancelar" onclick="fecharForm()" value="Cancelar" class="btn btn-gray py-2 px-4" hidden>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+  var id_user = "<?php echo $this->session->cidadao['doc']?>";
+  var email_user = "<?php echo $this->session->cidadao['login']?>";
+</script>
+
+<script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/editar_conta_cidadao.js"></script>
--- application/views/user_criadordesafio/minhaconta.php
+++ application/views/user_criadordesafio/minhaconta.php
@@ -1,63 +1,63 @@
-    <div class="site-section">
-      <div class="container mt-5">
-        <div class="row justify-content-center mb-5">
-          <div class="col-md-7 text-center border-primary">
-            <h2 class="font-weight-light text-primary">Minha conta</h2>
-          </div>
-        </div>
-
-        <div class="row justify-content-around mb-5">
-          <div class="col-10">
-            <form id="editar_conta">
-              <div id="alerta">
-
-              </div>
-
-              <div class="row form-group">
-                <div class="col-md-6 mb-3">
-                  <label class="text-black" for="nome">Nome</label>
-                  <input type="text" id="nome" name="nome" class="form-control" readonly>
-                </div>
-
-                <div class="col-md-6">
-                  <label class="text-black" for="email">E-mail</label>
-                  <input type="email" id="email" name="email" class="form-control" readonly>
-                </div>
-              </div>
-
-              <div class="row form-group">
-                <div class="col-md-4 mb-3">
-                  <label class="text-black" for="doc" id="lblDoc">Documento de identificação</label>
-                  <input type="text" id="doc" name="doc" class="form-control" readonly>
-                </div>
-
-                <div class="col-md-4 mb-3">
-                  <label class="text-black" for="cep">CEP</label>
-                  <input type="text" id="cep" name="cep" class="form-control" readonly>
-                </div>
-
-                <div class="col-md-4 mb-3">
-                  <label class="text-black" for="senha">Senha</label> 
-                  <input type="password" id="senha" name="senha" class="form-control" aria-describedby="passwordHelpBlock" readonly>
-                </div>
-              </div>
-
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <input type="button" id="editar" onclick="editarForm()" value="Editar informações" class="btn btn-primary py-2 px-4 text-white">
-                  <input type="submit" id="salvar" value="Salvar alterações" class="btn btn-primary py-2 px-4 text-white" hidden>
-                  <input type="button" id="cancelar" onclick="fecharForm()" value="Cancelar" class="btn btn-secondary py-2 px-4" hidden>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
+<div class="site-section">
+  <div class="container">
+    <div class="row justify-content-center mb-5">
+      <div class="col-10 col-md-8 text-center text-uppercase">
+        <h2 class="title-section">Minha conta</h2>
       </div>
     </div>
 
-    <script>
-      var id_user = "<?php echo $this->session->criador['doc']?>";
-      var email_user = "<?php echo $this->session->criador['login']?>";
-    </script>
+    <div class="row justify-content-around mb-5">
+      <div class="col-10">
+        <form id="editar_conta">
+          <div id="alerta">
 
-    <script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/editar_conta_criador.js"></script>
+          </div>
+
+          <div class="row form-group">
+            <div class="col-md-6 mb-3">
+              <label class="text_label" for="nome">Nome</label>
+              <input type="text" id="nome" name="nome" class="form-control" readonly>
+            </div>
+
+            <div class="col-md-6">
+              <label class="text_label" for="email">E-mail</label>
+              <input type="email" id="email" name="email" class="form-control" readonly>
+            </div>
+          </div>
+
+          <div class="row form-group">
+            <div class="col-md-4 mb-3">
+              <label class="text_label" for="doc" id="lblDoc">Documento de identificação</label>
+              <input type="text" id="doc" name="doc" class="form-control" readonly>
+            </div>
+
+            <div class="col-md-4 mb-3">
+              <label class="text_label" for="cep">CEP</label>
+              <input type="text" id="cep" name="cep" class="form-control" readonly>
+            </div>
+
+            <div class="col-md-4 mb-3">
+              <label class="text_label" for="senha">Senha</label> 
+              <input type="password" id="senha" name="senha" class="form-control" aria-describedby="passwordHelpBlock" readonly>
+            </div>
+          </div>
+
+          <div class="row form-group">
+            <div class="col-md-12">
+              <input type="button" id="editar" onclick="editarForm()" value="Editar informações" class="btn btn-green py-2 px-4 text-white">
+              <input type="submit" id="salvar" value="Salvar alterações" class="btn btn-green py-2 px-4 text-white" hidden>
+              <input type="button" id="cancelar" onclick="fecharForm()" value="Cancelar" class="btn btn-gray py-2 px-4" hidden>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+  var id_user = "<?php echo $this->session->criador['doc']?>";
+  var email_user = "<?php echo $this->session->criador['login']?>";
+</script>
+
+<script type="text/javascript" language="javascript" src="<?= base_url()?>assets/js/ajax/editar_conta_criador.js"></script>
